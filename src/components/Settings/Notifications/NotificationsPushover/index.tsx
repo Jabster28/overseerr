e32007dc73754dce24de70035095decbdc4c5cd6
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-04-29 22:21:02 -0400

    fix(ui): correct checkbox behavior

--- src/components/Settings/Notifications/NotificationsPushover/index.tsx
+++ src/components/Settings/Notifications/NotificationsPushover/index.tsx
@@ -237,6 +237,10 @@ const NotificationsPushover: React.FC = () => {
               onUpdate={(newTypes) => {
                 setFieldValue('types', newTypes);
                 setFieldTouched('types');
+
+                if (newTypes) {
+                  setFieldValue('enabled', true);
+                }
               }}
               error={
                 errors.types && touched.types
